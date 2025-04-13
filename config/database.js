0a13ea1d28bf20bbca5b94322adaf3ef3d59e753
luivasfer <luivasfer@gmail.com>
2025-04-13 11:22:17 -0400

    configuracion de base de datos añadido

--- config/database.js
+++ config/database.js
@@ -1,12 +1,20 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-
+console.log(process.env.DATABASE_DIALECT);
 const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  database: 'proyecto_final',
-  username: 'postgres',
-  password: 'dondeestas',
+  dialect: process.env.DATABASE_DIALECT,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+  
 });
 
 module.exports = sequelize;
